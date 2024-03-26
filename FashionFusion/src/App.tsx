29ebcff3a568aedc3fa10df365b91bd805e9a8e8
renuka <renukasjagrwal@gmail.com>
2024-03-26 19:23:35 +0530

    day-1 renuka

---
+++ FashionFusion/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Heading } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+ <Heading className="text-3xl font-bold underline bg-amber-300">
+      Hello world!
+    </Heading>
+    </>
+  )
+}
+
+export default App
