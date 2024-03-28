28100a358f2fe3393e208aaf219c64f2cb70743c
renuka <renukasjagrwal@gmail.com>
2024-03-29 00:57:02 +0530

    added Routing

--- FashionFusion/src/Redux/store.ts
+++ FashionFusion/src/Redux/store.ts
@@ -1,13 +1,8 @@
 // store.ts
 import { applyMiddleware, combineReducers, createStore, Store } from "redux";
 import {thunk} from "redux-thunk";
-<<<<<<< HEAD
 import { AuthReducer } from "./Reducers/AuthReducer";
 
-=======
-import { authReducer } from "./reduxer";
-import { dataReducer } from "./fetchMenDataReducer/dataReducer";
->>>>>>> a3ab43b96930f7618d449692943aea66457d4634
 
 
 interface RootState {
@@ -15,12 +10,7 @@ interface RootState {
 }
 
 const rootReducer = combineReducers<RootState>({
-<<<<<<< HEAD
   auth: AuthReducer,
-=======
-  auth: authReducer,
-  data: dataReducer,
->>>>>>> a3ab43b96930f7618d449692943aea66457d4634
 });
 
 const store: Store<RootState> = createStore(rootReducer, applyMiddleware(thunk));
