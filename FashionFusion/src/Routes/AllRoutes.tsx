1976638e3e71d104702b87e9b4a492707f2bf1bf
Aman Chaubey <amanchaubey86@gmail.com>
2024-03-30 20:15:58 +0530

    single product page

--- FashionFusion/src/Routes/AllRoutes.tsx
+++ FashionFusion/src/Routes/AllRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage";
 import Admin from "../Pages/Admin";
 import Error from "../Pages/Error";
+import SingleMen from "../Pages/SingleMen";
 
 function AllRoutes() {
   return (
@@ -10,6 +11,7 @@ function AllRoutes() {
       <Route path="/loginPage" element={<LoginPage />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/*" element={<Error />} />
+      <Route path="/products/:id" element={<SingleMen />} /> 
       {/* <Route path='/women' element={<Women/>} /> */}
       {/* <Route path='/singleWomen/:id' element={<singleWomen/>} /> */}
       {/* <Route path=/'men' element={<Men/>} /> */}
